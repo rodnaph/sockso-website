2188b18571c1b46734dc0ee5991d5712ee8b0ddf
rod <rod@pu-gh.com>
2011-03-18 15:32:56 +0000

    added some empty tests

---
+++ tests/php/controllers/Community/ControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../../tests/bootstrap.php';
+
+class Comunity_ControllerTest extends Smut_Tests_ControllerTestCase {
+
+    public function testIndexActionRendersListOfActiveServers() {
+        
+    }
+
+}
