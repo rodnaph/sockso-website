a8fe9311fc9812091e6027abf4a8d603e94856b6
rod <rod@pu-gh.com>
2011-05-06 18:51:43 +0100

    added displaying of recent commit data on source page

--- controllers/Source/Controller.class.php
+++ controllers/Source/Controller.class.php
@@ -1,9 +1,43 @@
 <?php
-
+/**
+ * Shows info about the Sockso source code
+ * 
+ */
 class Source_Controller extends Default_Controller {
 
-    private $commitsRssUrl = 'https://github.com/rodnaph/sockso/commits/master.atom';
+    const RECENT_COMMITS_URL = 'source.recentCommitsUrl';
+    const RECENT_COMMITS_CACHE_FILE = 'sourceCommits.cache';
+    const RECENT_COMMITS_CACHE_TIMEOUT = 3600;
 
+    /**
+     * @var Smut_Cache_CacheCreator
+     */
+    private $cacheCreator;
+
+    /**
+     * @var Smut_Config
+     */
+    private $config;
+
+    /**
+     * Set the cache creator object
+     *
+     * @inject
+     *
+     * @param Smut_Cache_Creator $cacheCreator
+     * @param Smut_Config $config
+     */
+    public function setDependencies( Smut_Cache_Creator $cacheCreator, Smut_Config $config ) {
+
+        $this->cacheCreator = $cacheCreator;
+        $this->config = $config;
+
+    }
+
+    /**
+     * Show the index page with source info and recent commits
+     * 
+     */
     public function doIndex() {
 
         $this->render( 'index', array(
@@ -12,14 +46,25 @@ class Source_Controller extends Default_Controller {
         
     }
 
+    /**
+     * Fetches simple xml object of recent commit data
+     *
+     * @return SimpleXml
+     */
     private function getRecentCommits() {
-        
-        $data = $this->getUrlData( $this->commitsRssUrl );
-        $xml = simplexml_load_string( $data );
-        $commits = array();
 
+        $url = $this->config->get( self::RECENT_COMMITS_URL );
+        $cache = $this->cacheCreator->getFileCache(
+            self::RECENT_COMMITS_CACHE_FILE,
+            self::RECENT_COMMITS_CACHE_TIMEOUT
+        );
 
-        return $commits;
+        if ( !$cache->isCached() ) {
+            $data = $this->getUrlData( $url );
+            $cache->write( $data );
+        }
+
+        return simplexml_load_string( $cache->read() );
 
     }
 
--- views/controllers/source/index.tpl
+++ views/controllers/source/index.tpl
@@ -3,20 +3,38 @@
 
 <div id="welcome">
 
-	<h1>Source Code</h1>
+    <h1>Source Code</h1>
 
-	<p>Sockso uses loads of brilliant source code and free libraries
-	that you can find linked to on the right of the page.  Without lots of other
-	peoples hard work this project would have been too much, but with the freedom
-	of the open source community, and the help of others, we can make great things!</p>
-	
-	<p>I'm using <a href="http://github.com">Github</a> for source control
-	with Sockso.  You can get a copy of sockso using the following command:</p>
-	
-	<p><pre>git clone https://github.com/rodnaph/sockso.git</pre></p>
+    <p>Sockso uses loads of brilliant source code and free libraries
+    that you can find linked to on the right of the page.  Without lots of other
+    peoples hard work this project would have been too much, but with the freedom
+    of the open source community, and the help of others, we can make great things!</p>
 
-        <p>I push my latest changes here pretty often so it should be up to date with
-        the latest development.</p>
+    <p>I'm using <a href="http://github.com">Github</a> for source control
+    with Sockso.  You can get a copy of sockso using the following command:</p>
+
+    <p><pre>git clone https://github.com/rodnaph/sockso.git</pre></p>
+
+    <p>I push my latest changes here pretty often so it should be up to date with
+    the latest development.</p>
+
+    <div class="recentChanges">
+
+        <h2>Recent Changes</h2>
+
+        <ul class="commits">
+
+            {foreach $commits->entry as $entry}
+            <li class="commit">
+                <span class="title">{$entry->title}</span>
+                -
+                <span class="author">{$entry->author->name}</span>
+            </li>
+            {/foreach}
+
+        </ul>
+
+    </div>
 
 </div>
 
